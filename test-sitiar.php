7fa340a5829b9b1e4d79b747bb5899a68c06a5f2
BVAk <bekkviktoria55@gmail.com>
2020-04-14 18:14:04 +0300

    layout grid

--- test-sitiar.php
+++ test-sitiar.php
@@ -5,15 +5,15 @@ if (file_exists('Test-sitiar.xml')) {
     echo "<div class='catalog'> ";
     foreach ($shop->xpath('//pos') as $pos) {
         $desc = str_replace(array('<descr>', '</descr>'), '', $pos->descr->asXML());
+        $price = floatval($pos->price);
+        $price = number_format($price, 0, ' ', ' ');
         echo <<<XML
 <div class="pos" id="{{$pos->uid}}"><img src="css/table.png">
 <hr class="line"></hr>
 <div class="text">
-
  <div class="title">{$pos->title}</div>
-
  <div class="description">{$desc}</div>
- <div class="price">{$pos->price}</div> </div>
+ <div class="price">{$price} грн.</div> </div>
  </div>
 XML;
     }
